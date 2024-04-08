63c77dbb48d9dc76140ebdad41f419c184e97549
victor kamau <vikakamau04@gmail.com>
2024-04-08 14:15:40 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 // Your code here
-let movie = "http://localhost:3000/films"
+let movie = "http://localhost:3000/films"// shows the localhost used
+//uses dom to declare function
 document.addEventListener('DOMContentLoaded', async(event)=>{
 const data = await showFilms()
 viewPoster(data)
+movieTitle(data)
+calculateTickets(data)
 })
 ticketsFilm()
+
 function showFilms(){
     return fetch(movie,{
         method:"GET",
@@ -32,6 +36,37 @@ function ticketsFilm(){
     }))
 }
 
+function movieTitle(data){
+    const titles = document.getElementById("showing")
+    const display=document.querySelectorAll(".photos")
+    display.forEach(action =>{
+        action.addEventListener("click", (event)=>{
+            const watch = data.find((element)=> element.id===event.target.id)
+            titles.innerHTML=`
+            <div class="card">
+            <div id="title" class="title">${watch.title}</div>
+            <div id="runtime" class="meta">${watch.runtime} minutes</div>
+            <div class="content">
+              <div class="description">
+                <div id="film-info">${watch.description}</div>
+                <span id="showtime" class="ui label">${watch.showtime}</span>
+                <span id="ticket-num">${watch.capacity-watch.tickets_sold}</span> remaining tickets
+              </div>
+            </div>
+            <div class="extra content">
+              <button id="buy-ticket" class="ui orange button">
+                Buy Ticket
+              </button>
+            </div>
+            </div>
+        
+            `
+            
+        })
+    })
+
+}
+
 function viewPoster(data){
     const live = document.querySelector('#live')
     const cont = document.createElement('div')
@@ -42,23 +77,33 @@ function viewPoster(data){
       cont.innerHTML =`
       <img src=${Acquiredfilm.poster}>`
       live.appendChild(cont)
+
     })
    })
   }
 
-  function viewMovie(){
-return fetch (movie)
-.then(res => res.json())
-.then(films =>films.map(film=>{
-    return`
-    const title = ${film.title}
-    const runtime = ${film.runtime}
-    const description = ${film.description}
-    const showtime = ${film.showtime}
-    const  capacity = ${film.capacity}
-    const ticketSold = ${film.ticket_sold}
-    const availableTickets = ${capacity-ticket_sold}   `
-}))
+
+  
 
 
-  }
+function calculateTickets(data){
+    let availableTickets= document.getElementById("ticket-num");
+    let ticketsBought = document.getElementById("buy-ticket")
+    const buttonviews= document.querySelectorAll("button")
+    buttonviews.forEach(btn=> {
+        btn.addEventListener("click", (event) => {
+            let readyTickets = parseInt(availableTickets.innerHTML);
+            if(readyTickets>0){
+                readyTickets--;
+                availableTickets.innerHTML= readyTickets.toString();
+                if(readyTickets===0){
+                    availableTickets.textContent = "0"
+                    ticketsBought.disabled = true; 
+                    console.log("sold out")
+                    ticketsBought.appendChild(availableTickets)
+                }
+            }
+        console.log
+        })
+    });
+}
